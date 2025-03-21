c5a12900fa7452524b16524797831edd4b66e764
impactice <107308111+impactice@users.noreply.github.com>
2025-03-21 13:03:41 +0900

    Add files via upload

---
+++ 0321/hello.c
@@ -0,0 +1,7 @@
+#include<stdio.h>
+
+int void()
+{
+printf("Hello world!");
+return0;
+}
---
+++ 0321/mission.c
@@ -0,0 +1,10 @@
+#include<stdio.h> 
+
+int main(){
+int num;
+char na[100];
+scanf("%d",&num);
+scanf("%s",na);
+printf("학번 : %d, 이름: %s",num,na);
+return 0;
+}
